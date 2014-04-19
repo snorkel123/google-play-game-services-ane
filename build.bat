2d5756d36ef8d5935cf0941fdca00f77b595d222
alextel69 <alextel69@gmail.com>
2014-04-19 23:33:56 +0400

    Add ant build files

---
+++ build.bat
@@ -0,0 +1,4 @@
+:: CHANGE TO YOUR OWN PATHS
+set ANT_PATH=C:\Program Files\adt-bundle-windows\eclipse\plugins\org.apache.ant_1.8.3.v201301120609\bin\ant
+set BUILD_PATH=D:\Git\google-play-game-services-ane\build.xml
+"%ANT_PATH%" -f "%BUILD_PATH%"
